8f925211044808d0413cd4912ddbeef673df5c74
nebocco <nebocco@gmail.com>
2021-07-05 14:34:46 +0900

    for deploy

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,22 @@
 
 use std::env;
 use actix_backend::server::run_server;
-use actix_backend::config::db;
+use sql_client::create_pool;
 
 #[actix_web::main]
 async fn main() {
     dotenv::dotenv().ok();
     std::env::set_var("RUST_LOG", "actix_web=debug");
-    std::env::set_var("RUST_LOG", "info");
     env_logger::init();
 
-    let database_url = env::var("SQL_URL").expect("SQL_URL is not set.");
-    let port: u16 = 8008;
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set.");
+    let host: String = std::env::var("HOST").expect("HOST is not set.");
+    let port: u16 = std::env::var("PORT_API").expect("PORT_API is not set.")
+        .parse::<u16>().unwrap();
 
-    let pg_pool = db::create_and_initialize_pool(&database_url)
+    let pg_pool = create_pool(&database_url)
         .await
         .expect("Failed to initialize the connection pool");
 
-    run_server(pg_pool, port).await.expect("Failed to run server");
+    run_server(pg_pool, host.as_ref(), port).await.expect("Failed to run server");
 }
